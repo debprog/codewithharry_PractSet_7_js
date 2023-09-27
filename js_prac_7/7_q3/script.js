146c6434d30fb2f3ad9465c79b2e4c275ff23822
debprog <35360322+debprog@users.noreply.github.com>
2023-09-27 11:38:12 +0530

    Add files via upload

---
+++ js_prac_7/7_q3/script.js
@@ -0,0 +1,4 @@
+parent=document.getElementById("parent");
+
+parent.firstElementChild.style.color="green";
+parent.lastElementChild.style.color="green";
---
+++ js_prac_7/7_q4/script.js
@@ -0,0 +1,9 @@
+arrayOfli=document.getElementsByTagName("li");
+lis=document.querySelectorAll("li");
+
+console.log(lis);
+
+
+lis.forEach((element) => {
+
+    element.style.background="red";});
